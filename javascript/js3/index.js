809721d23d37dc2c9708a869b175b6a17ab528b7
Anan Nasreen <anan.nasreen@vonnue.com>
2022-12-13 20:02:16 +0530

    feat javascript

--- javascript/js3/index.js
+++ javascript/js3/index.js
@@ -124,18 +124,19 @@
 // const averageAge = ageJonas + ageSarah / 2;
 // console.log(ageJonas, ageSarah, averageAge)
 
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
-console.log(ageJonas, ageSarah);
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
 
-console.log(ageJonas * 2, ageSarah / 10, 2 ** 3);
+// console.log(ageJonas * 2, ageSarah / 10, 2 ** 3);
 
-const firstName = 'Jonas';
-const lastName = 'Schmedtmann';
-console.log(firstName + ' ' + lastName);
+// const firstName = 'Jonas';
+// const lastName = 'Schmedtmann';
+// console.log(firstName + ' ' + lastName);
 
 let x = 10+5;
 x += 10;
 x += 4;
-console.log(x)
+console.log(x);
+
