f37485d52ba9167d3b7e85e6f08a670d9db06861
Anan Nasreen <anan.nasreen@vonnue.com>
2022-12-13 18:47:26 +0530

    feat: javascript practised

---
+++ javascript/js3/index.js
@@ -0,0 +1,141 @@
+// const now = 2037;
+// const ageJonas = now -1991;
+// const ageSarah = now - 2018;
+
+// let x,y;
+// x=y=25 - 10 - 5;
+// console.log(x,y);
+
+// const avgAge = (ageJonas + ageSarah) / 2
+// console.log(ageJonas,ageSarah,avgAge)
+
+// let massMark = 78;
+// let heightMark = 1.69;
+// let massJohn = 92;
+// let heightJohn = 1.95;
+
+// let massMark = 95;
+// let heightMark = 1.88;
+// let massJohn = 85;
+// let heightJohn = 1.76;
+
+// const bmiMark = massMark / heightMark**2;
+// const bmiJohn = massJohn / heightJohn**2;
+// const markHigherBMI = bmiMark > bmiJohn;
+// console.log(bmiMark, bmiJohn, markHigherBMI);
+
+// const firstName  = 'Jonas';
+// const job = 'teacher';
+// const birthYear = 1991;
+// const year = 2037;
+// const jonas = "I'm" + " " + firstName + ', a' + " " +(year - birthYear) + " " + 'years old teacher!'
+// console.log(jonas)
+
+// const age = 19;
+// const isOldEnough = age >= 18;
+
+// if(isOldEnough) {
+//    console.log('Sarah can start driving license')
+// } 
+
+// let massMark = 95;
+// let heightMark = 1.88;
+// let massJohn = 85;
+// let heightJohn = 1.76;
+
+// const bmiMark = massJohn / heightJohn**2;
+// const bmiJohn = massJohn / heightJohn**2;
+
+// console.log(bmiJohn, bmiMark)
+//  const markHigherBMI = bmiMark > bmiJohn;
+
+// if (bmiMark > bmiJohn) {
+//     console.log(`Mark's BMI ${bmiMark} is higher then John's ${bmiJohn}`)
+// } else {
+//     console.log(`John's BMI ${bmiJohn} is higher then Mark's ${bmiMark})`)
+// }
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('Jonas'));
+// console.log(Boolean({}));
+// console.log(Boolean(''));
+
+// const money = NaN;
+// if (money) {
+//     console.log("Dont spend it all;");
+// } else {
+//     console.log("You should get a job!");
+// }
+
+// let height;
+// if (height) {
+//     console.log('YAY! Height is defined');
+// } else {
+//     console.log('Height is UNDEFINED');
+// }
+
+//  height = 30;
+// if (height) {
+//     console.log('YAY! Height is defined');
+// } else {
+//     console.log('Height is UNDEFINED');
+// }
+
+// const age = 18;
+// if (age === 18) console.log('You just became an adult')
+
+// const inputYear = '1991';
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear), + 18);
+
+// console.log(Number('Jonas'));
+// console.log(typeof NaN);
+
+// console.log(String(23), 23);
+
+// console.log('I am ' + 23 + ' years old');
+// console.log('23' - '10' - 3);
+
+// console.log('23' / '2');
+
+// let n = '1' + 1;
+// n = n-1;
+// console.log(n)
+
+// const birthYear = 2012;
+// let century;
+
+// if (birthYear <= 2000) {
+//     century = 20;
+// }else {
+//     century = 21;
+// }
+// console.log(century);
+
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+
+// let x,y;
+// x = y = 25 - 10 - 5;
+// console.log(x,y);
+
+// const averageAge = ageJonas + ageSarah / 2;
+// console.log(ageJonas, ageSarah, averageAge)
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageSarah / 10, 2 ** 3);
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+let x = 10+5;
+x += 10;
+x += 4;
+console.log(x)
