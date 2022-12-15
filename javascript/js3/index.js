0feffc81a2fe65ccb65aabe9ecde59bd8a2b66c7
Anan Nasreen <anan.nasreen@vonnue.com>
2022-12-15 17:20:04 +0530

    feat: javascript

--- javascript/js3/index.js
+++ javascript/js3/index.js
@@ -1,4 +1,4 @@
-// 'use strict'
+'use strict';
 
 // let hasDriversLisense = false;
 // const passTest = true;
@@ -45,7 +45,7 @@
 
 // if(isOldEnough) {
 //    console.log('Sarah can start driving license')
-// } 
+// }
 
 // let massMark = 95;
 // let heightMark = 1.88;
@@ -59,10 +59,10 @@
 //  const markHigherBMI = bmiMark > bmiJohn;
 
 // if (bmiMark > bmiJohn) {
-//     console.log(`Mark's BMI ${bmiMark} 
+//     console.log(`Mark's BMI ${bmiMark}
 // is higher then John's ${bmiJohn}`)
 // } else {
-//     console.log(`John's BMI ${bmiJohn} 
+//     console.log(`John's BMI ${bmiJohn}
 // is higher then Mark's ${bmiMark})`)
 // }
 
@@ -161,7 +161,7 @@
 //     case 'tuesday':
 //         console.log('Prepare theory videos');
 //         break;
-//     case 'wednesday': 
+//     case 'wednesday':
 //     case 'thursday':
 //         console.log('Write code examples');
 //         break;
@@ -192,7 +192,7 @@
 // }
 
 //  const age =23;
-//  age >= 18 ? console.log('I like to drink wine'): 
+//  age >= 18 ? console.log('I like to drink wine'):
 //  console.log('I like to drink water')
 
 // const age = 19;
@@ -293,21 +293,174 @@
 // console.log(yearsUnitRetirement(1991, 'Jonas'));
 // console.log(yearsUnitRetirement(1950, 'Mike'));
 
-const calcAverage = (a , b , c) => (a + b + c) / 3;
-console.log(calcAverage(3 , 4, 5));
+// const calcAverage = (a , b , c) => (a + b + c) / 3;
+// console.log(calcAverage(3 , 4, 5));
 
-const scoreDolphins = calcAverage(44 , 23 , 71);
-const scoreKoalas = calcAverage(65 , 54 , 49)
+// const scoreDolphins = calcAverage(44 , 23 , 71);
+// const scoreKoalas = calcAverage(65 , 54 , 49)
 
-console.log(scoreDolphins, scoreKoalas);
+// console.log(scoreDolphins, scoreKoalas);
 
-const checkWinner = function(avgDolphins, avgKoalas) {
-    if (avgDolphins >= 2*avgKoalas) {
-        console.log(`Dolphins win (${avgDolphins}) vs. (${avgKoalas})`);
-    } else if (avgKoalas >= 2*avgDolphins) {
-        console.log(`Koalas win (${avgKoalas}) vs. (${avgDolphins})`);
-    } else {
-        console.log('No team wins...');
-    }
-}
-checkWinner(scoreDolphins,scoreKoalas);
+// const checkWinner = function(avgDolphins, avgKoalas) {
+//     if (avgDolphins >= 2*avgKoalas) {
+//         console.log(`Dolphins win (${avgDolphins}) vs. (${avgKoalas})`);
+//     } else if (avgKoalas >= 2*avgDolphins) {
+//         console.log(`Koalas win (${avgKoalas}) vs. (${avgDolphins})`);
+//     } else {
+//         console.log('No team wins...');
+//     }
+// }
+// checkWinner(scoreDolphins,scoreKoalas);
+
+// const jonas = {
+//     firstName: 'Jonas',
+//     lastName: 'Schmedtmann',
+//     age: 2037 -  1991,
+//     job: 'Teacher',
+//     friends: ['Micheal', 'Peter', 'Steven']
+// };
+
+// console.log(jonas);
+
+// console.log(jonas.lastName);
+// console.log(jonas['lastName']);
+
+// const nameKey = 'Name';
+// console.log(jonas['first' + nameKey]);
+// console.log(jonas['last'  + nameKey]);
+
+// let rep = 1;
+// while (rep <= 10) {
+//     console.log(`Lifting weights repetition ${rep}`);
+//     rep++;
+// }
+
+// let dice = Math.trunc(Math.random() * 6) + 1;
+// console.log(dice);
+
+// while (dice != 6) {
+//   console.log(`You rolled a ${dice}`);
+//   dice = Math.trunc(MAth.random() * 6) + 1;
+//   if (dice === 6) console.log("Loop is about to end....");
+// }
+
+// const calcAge3 = birthYeah => 2037 - birthYeah;
+// const age3 = calcAge3(1991);
+// console.log(age3);
+// const yearsUntilRetirement = (birthYeah, firstName) => {
+//   const age = 2037 - birthYeah;
+//   const retirement = 65 - age;
+//   // return retirement;
+//   return `${firstName} retires in ${retirement} years`;
+// };
+// console.log(yearsUntilRetirement(1991, 'Jonas'));
+// console.log(yearsUntilRetirement(1980, 'Bob'));
+
+// const calcAge = function (birthYeah) {
+//   return 2037 - birthYeah;
+// };
+// const yearsUntilRetirement = function (birthYeah, firstName) {
+//   const age = calcAge(birthYeah);
+//   const retirement = 65 - age;
+//   if (retirement > 0) {
+//     console.log(`${firstName} retires in ${retirement} years`);
+//     return retirement;
+//   } else {
+//     console.log(`${firstName} has already retired`);
+//     return -1;
+//   }
+// };
+// console.log(yearsUntilRetirement(1991, 'Jonas'));
+// console.log(yearsUntilRetirement(1950, 'Mike'));
+
+// const friend1 = 'Michael';
+// const friend2 = 'Steven';
+// const friend3 = 'Peter';
+// const friends = ['Michael', 'Steven', 'Peter'];
+// console.log(friends);
+// const y = new Array(1991, 1984, 2008, 2020);
+// console.log(friends[0]);
+// console.log(friends[2]);
+// console.log(friends.length);
+// console.log(friends[friends.length - 1]);
+// friends[2] = 'Jay';
+// console.log(friends);
+// // friends = ['Bob', 'Alice']
+// const firstName = 'Jonas';
+// const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+// console.log(jonas);
+// console.log(jonas.length);
+
+// const calcAge = function (birthYeah) {
+//   return 2037 - birthYeah;
+// };
+// const years = [1990, 1967, 2002, 2010, 2018];
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[years.length - 1]);
+// console.log(age1, age2, age3);
+// const ages = [
+//   calcAge(years[0]),
+//   calcAge(years[1]),
+//   calcAge(years[years.length - 1]),
+// ];
+// console.log(ages);
+
+// const jonas = {
+//   firstName: 'Jonas',
+//   lastName: 'Schmedtmann',
+//   age: 2037 - 1991,
+//   job: 'teacher',
+//   friends: ['Michael', 'Peter', 'Steven'],
+// };
+// console.log(jonas);
+// console.log(jonas.lastName);
+// console.log(jonas['lastName']);
+// const nameKey = 'Name';
+// console.log(jonas['first' + nameKey]);
+// console.log(jonas['last' + nameKey]);
+// const interestedIn = prompt(
+//   'What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends'
+// );
+// if (jonas[interestedIn]) {
+//   console.log(jonas[interestedIn]);
+// } else {
+//   console.log(
+//     'Wrong request! Choose between firstName, lastName, age, job, and friends'
+//   );
+// }
+// jonas.location = 'Portugal';
+// jonas['twitter'] = '@jonasschmedtman';
+// console.log(jonas);
+// console.log(
+//   `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`
+// );
+
+// const jonas = {
+//   firstName: 'Jonas',
+//   lastName: 'Schmedtmann',
+//   birthYeah: 1991,
+//   job: 'teacher',
+//   friends: ['Michael', 'Peter', 'Steven'],
+//   hasDriversLicense: true,
+//   // calcAge: function (birthYeah) {
+//   //   return 2037 - birthYeah;
+//   // }
+//   // calcAge: function () {
+//   //   // console.log(this);
+//   //   return 2037 - this.birthYeah;
+//   // }
+//   calcAge: function () {
+//     this.age = 2037 - this.birthYeah;
+//     return this.age;
+//   },
+//   getSummary: function () {
+//     return `${this.firstName} is a ${this.calcAge()}-year old ${
+//       jonas.job
+//     }, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`;
+//   },
+// };
+// console.log(jonas.calcAge());
+// console.log(jonas.age);
+// console.log(jonas.age);
+// console.log(jonas.age);
