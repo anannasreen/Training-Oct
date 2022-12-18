fe9e6f0665da0a8784a5d49096ee46d4fa2d76aa
Anan Nasreen <anan.nasreen@vonnue.com>
2022-12-18 14:53:03 +0530

    feat: javascript

--- javascript/js3/index.js
+++ javascript/js3/index.js
@@ -1,10 +1,10 @@
 'use strict';
 
-// const modal = document.querySelector('.modal');
-// const overlay = document.querySelector('.overlay');
-// const btnCloseModal = document.querySelector('.close-modal');
-// const btnsOpenModal = document.querySelectorAll('.show-modal');
-// console.log(btnsOpenModal);
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
 
 // for (let i = 0; i < btnsOpenModal.length; i++)
 //   btnsOpenModal[i].addEventListener('click', function () {
