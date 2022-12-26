30b36d21abd5d4e8f167ffbbe7e0ff256da19a84
Anan Nasreen <anan.nasreen@vonnue.com>
2022-12-26 11:50:13 +0530

    feat: Javascript

--- javascript/js3/index.js
+++ javascript/js3/index.js
@@ -5,7 +5,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -15,17 +16,25 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
+btnsOpenModal.forEach(btn => btn.addEventListener('click', closeModal));
+
 // for (let i = 0; i < btnsOpenModal.length; i++)
 //   btnsOpenModal[i].addEventListener('click', openModal);
 
-// btnCloseModal.addEventListener('click', closeModal);
-// overlay.addEventListener('click', closeModal);
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
-// document.addEventListener('keydown', function (e) {
-//   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-//     closeModal();
-//   }
-// });
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+console.log(document.documentElement);
+console.log(document.head);
+
+// const btnScrollTo=document.querySelector('.btn--scroll-to')
+
+////////////////////////////////////////////////////
 
 // const startLogOutTimer = function () {
 //   let time = 100;
