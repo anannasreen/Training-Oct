00c537a658c13f46c055059e6be6a826d75357e8
Anan Nasreen <anan.nasreen@vonnue.com>
2022-12-29 16:43:46 +0530

    feat: Javascript

--- javascript/js3/index.js
+++ javascript/js3/index.js
@@ -24,6 +24,50 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 // let map, mapEvent;
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+
+  constructor(coords, distance, duration) {
+    // this.date=...
+    // this.id=...
+    this.coords = coords;
+    this.distance = distance;
+    this.duration = duration;
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+// const run1 = new Running([39, -12], 5.2, 24, 178);
+// const cycling1 = new Cycling([39, -12], 27, 95, 523);
+// console.log(run1, cycling1);
+
+// class Cycling extends Workout {}
 
 class App {
   #map;
@@ -109,6 +153,7 @@ class App {
 }
 
 const app = new App();
+
 // app._getPosition();
 
 // class CarCl {
@@ -2961,3 +3006,4 @@ const app = new App();
 //   console.log(str);
 // };
 // printForCast(data1);
+
