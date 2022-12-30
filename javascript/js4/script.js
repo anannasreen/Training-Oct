ebb1e3d424c9a3e9fd194221edfc7c4a2824cbba
Anan Nasreen <anan.nasreen@vonnue.com>
2022-12-30 19:22:32 +0530

    feat: Javascript

--- javascript/js4/script.js
+++ javascript/js4/script.js
@@ -208,6 +208,11 @@
 // };
 
 // btn.addEventListener('click', whereAmI);
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
 
 const imgContainer = document.querySelector('.images');
 
@@ -226,8 +231,24 @@ const createImage = function (imgPath) {
   });
 };
 
+let currentImg;
+
 createImage('img/img-1.jpg')
   .then(img => {
+    currentImg = img;
     console.log('Image 1 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image 1 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
   })
   .catch(err => console.error(err));
