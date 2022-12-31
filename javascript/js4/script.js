c195111d71e2ea7266e5d51a59cddd09489f8cd1
Anan Nasreen <anan.nasreen@vonnue.com>
2022-12-31 17:12:38 +0530

    Javascript

--- javascript/js4/script.js
+++ javascript/js4/script.js
@@ -388,4 +388,45 @@
 
 // console.log('Importing module');
 
-addToCart('bread', 5);
+// addToCart('bread', 5);
+// import cloneDeep from 'lodash-es';
+
+// const state = {
+//   cart: [
+//     { product: 'bread', quantity: 5 },
+//     { product: 'pizza', quantity: 5 },
+//   ],
+//   user: { loggedIn: true },
+// };
+// const stateClone = Object.assign({}, state);
+// const stateDeepClone = cloneDeep(state);
+
+// state.user.loggedIn = false;
+// console.log(stateClone);
+
+// console.log(stateDeepClone);
+
+// if (module.hot) {
+//   module.hot.accept();
+// }
+
+// class Person {
+//   #greeting = 'Hey';
+//   constructor(name) {
+//     this.name = name;
+//     console.log(`${this.#greeting}, ${this.name}`);
+//   }
+// }
+// const jonas = new Person('Jonas');
+
+// console.log('Jonas' ?? null);
+
+// console.log(cart.find(el => el.quantity >= 2));
+// Promise.resolve('TEST').then(x => console.log(x));
+
+// import 'core-js/stable';
+// // import 'core-js/stable/array/find';
+// // import 'core-js/stable/promise';
+
+// // Polifilling async functions
+// import 'regenerator-runtime/runtime';
--- javascript/js4/shoppingCart.js
+++ javascript/js4/shoppingCart.js
@@ -1,9 +1,19 @@
-console.log('Exporting module');
+// console.log('Exporting module');
 
-const shippingCart = 10;
-const cart = [];
+// const shippingCart = 10;
+// const cart = [];
 
-export const addToCart = function (product, quantity) {
+// export const addToCart = function (product, quantity) {
+//   cart.push({ product, quantity });
+//   console.log(`${quantity}${product} added to cart`);
+// };
+
+const totalPrice = 237;
+const totalQuantity = 23;
+
+export { totalPrice, totalQuantity as tq };
+
+export default function (product, quantity) {
   cart.push({ product, quantity });
-  console.log(`${quantity}${product} added to cart`);
-};
+  console.log(`${quantity} ${product} added to cart`);
+}
