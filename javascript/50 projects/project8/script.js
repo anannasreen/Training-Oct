dc0989f0bd9b61ee129222c08a8fd55b3a9e7281
Anan Nasreen <anan.nasreen@vonnue.com>
2023-01-04 14:32:12 +0530

    feat: Javascript 50 projects

--- javascript/50 projects/project8/script.js
+++ javascript/50 projects/project8/script.js
@@ -12,7 +12,7 @@ smallCups.forEach((cup, idx) => {
 function highlightCups(idx) {
   if (
     smallCups[idx].classList.contains('full') &&
-    !smallCups[idx].nextElementSibling.classList.contains('full')
+    !smallCups[idx].nextElementSibling.classList.contains('.full')
   ) {
     idx--;
   }
@@ -35,6 +35,17 @@ function updateBigCup() {
   if (fullCups === 0) {
     percentage.style.visibility = 'hidden';
     percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = 'visible';
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+  if (fullCups === totalCups) {
+    remained.style.visibility = 'hidden';
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = 'visible';
+    litres.innerText = `${2 - (250 * fullCups) / 1000}`;
   }
 }
 
