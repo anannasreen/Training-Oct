69aabb8d71e21df6a1573f813cdf29d64168e5a8
Anan Nasreen <anan.nasreen@vonnue.com>
2023-01-04 10:12:38 +0530

    feat: Javascript 50 projects

--- javascript/50 projects/project8/script.js
+++ javascript/50 projects/project8/script.js
@@ -1,3 +1,11 @@
+const toggles = document.querySelectorAll('.faq-toggle');
+
+toggles.forEach(toggle => {
+  toggle.addEventListener('click', () => {
+    toggle.parentNode.classList.toggle('active');
+  });
+});
+
 // const insert = document.getElementById('insert');
 
 // window.addEventListener('keydown', event => {
